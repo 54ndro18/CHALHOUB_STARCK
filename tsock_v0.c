e89cf2c4679812819c6f7c58326ac03b11b622a2
54ndro18 <42498137+54ndro18@users.noreply.github.com>
2022-02-10 13:30:55 +0100

    Add files via upload

--- tsock_v0.c
+++ tsock_v0.c
@@ -18,18 +18,126 @@ données du réseau */
 /* pour la gestion des erreurs */
 #include <errno.h>
 
-void main (int argc, char **argv)
+void construire_message(char * message, char motif, int lg) {
+	int i;
+	for (i=0;i<lg;i++)
+		message[i] = motif;
+}
+
+void afficher_message(char *message, int lg) {
+	int i;
+	printf("message construit : ");
+	for (i=0;i<lg;i++)
+		printf("%c", message[i]);
+	printf("\n");
+}
+
+void source_udp(int port, char * host, int nb_message, int lg_M) { // ça marchouille à priori
+	// Creation du socket local
+	int sock;
+	if ((sock = socket(AF_INET, SOCK_DGRAM, 0)) == -1) {
+		printf("Echec de creation du socket \n");
+		exit(1);
+	}
+	// Construction de l'adresse du socket destinataire
+	struct hostent * hp;
+	struct sockaddr_in adr_distant;
+	int lg_adr_distant = sizeof(adr_distant);
+	memset((char *)& adr_distant, 0, sizeof(adr_distant)) ;
+	adr_distant.sin_family = AF_INET ; // domaine Internet
+	adr_distant.sin_port = port ; // n° de port
+
+	if ((hp = gethostbyname(host)) == NULL) { 
+		printf("erreur gethostbyname\n");
+		exit(1);
+	}
+	memcpy((char*)&(adr_distant.sin_addr.s_addr), hp->h_addr, hp->h_length);
+	
+	
+	// Emission du message au destinaire
+	for (int i = 0; i < nb_message;i++) {
+		// Creation message
+		char * message = malloc(lg_M*sizeof(char));
+		construire_message(message,(char)(i%26 + 97),lg_M);
+		printf("%c",(char)(i%26 + 97));
+		// Envoi du message
+		sendto(sock, message, lg_M, 0, ((struct sockaddr *)&adr_distant), lg_adr_distant);
+		free(message);
+	}
+
+	// Destruction du socket
+	if (close(sock) == -1) {
+		printf("Echec de destructon du socket \n");
+		exit(1);
+	}
+	
+}
+/*
+void puit_udp(int port) { // EN CHANTIER !
+	// Creation du socket local
+	int sock;
+	if ((sock = socket(AF_INET, SOCK_DGRAM, 0)) == -1) {
+		printf("Echec de creation du socket \n");
+		exit(1);
+	}
+
+	
+	struct sockaddr_in adr_local ; // Adresse du socket local
+	int lg_adr_local = sizeof(adr_local) ;
+
+	// Construction de l’adresse du socket
+	memset((char*)&adr_local, 0, sizeof(adr_local));
+	adr_local.sin_family = AF_INET;
+	adr_local.sin_port = port;
+	adr_local.sin_addr.s_addr = INADDR_ANY;
+
+	// Association de l'adresse socket et représentation interne
+	if (bind (sock, (struct sockaddr *)&adr_local, lg_adr_local) == -1) {
+		printf("échec du bind\n");
+		exit(1);
+	}
+
+	// Réception du message
+	int bcp_de_place = ENTIER_VRAIMENT_GRAND;
+	char * message = malloc(bcp_de_place);
+	int temp;
+	if ((temp = int recvfrom(sock, pmesg, lg_max, 0, padr_em, plg_adr_em)) == -1) {
+		printf("Echec de réception \n");
+	}
+
+	// Destruction du socket
+	if (close(sock) == -1) {
+		printf("Echec de destructon du socket \n");
+		exit(1);
+	}
+			
+}
+*/
+
+void source_tcp() {
+
+}
+
+void puit_tcp() {
+
+}
+
+int main (int argc, char **argv)
 {
 	int c;
 	extern char *optarg;
 	extern int optind;
 	int nb_message = -1; /* Nb de messages à envoyer ou à recevoir, par défaut : 10 en émission, infini en réception */
 	int source = -1 ; /* 0=puits, 1=source */
-	while ((c = getopt(argc, argv, "pn:s")) != -1) {
+	int protocol = -1; // 0 = UDP, 1 = TCP
+	int port = htons(atoi(argv[argc - 1]));
+	char * host = argv[argc - 2];
+	int lg_M = 30;
+	while ((c = getopt(argc, argv, "upn:s")) != -1) {
 		switch (c) {
 		case 'p':
 			if (source == 1) {
-				printf("usage: cmd [-p|-s][-n ##]\n");
+				printf("usage: cmd [-p|-s][-u][-n ##]\n");
 				exit(1);
 			}
 			source = 0;
@@ -37,7 +145,7 @@ void main (int argc, char **argv)
 
 		case 's':
 			if (source == 0) {
-				printf("usage: cmd [-p|-s][-n ##]\n");
+				printf("usage: cmd [-p|-s][-u][-n ##]\n");
 				exit(1) ;
 			}
 			source = 1;
@@ -46,22 +154,51 @@ void main (int argc, char **argv)
 		case 'n':
 			nb_message = atoi(optarg);
 			break;
-
+		
+		case 'u':
+			protocol = 0;
 		default:
-			printf("usage: cmd [-p|-s][-n ##]\n");
+			printf("usage: cmd [-p|-s][-u][-n ##]\n");
 			break;
 		}
 	}
 
 	if (source == -1) {
-		printf("usage: cmd [-p|-s][-n ##]\n");
+		printf("usage: cmd [-p|-s][-u][-n ##]\n");
 		exit(1) ;
 	}
 
-	if (source == 1)
+	if (source == 1) {
 		printf("on est dans le source\n");
-	else
+		switch (protocol) {
+			case  0:
+				source_udp(port, host, nb_message, lg_M);
+				break;
+
+			case 1:
+				printf("Coucou");
+				break;
+				
+			default:
+				printf("Coucou");
+				break;
+		}
+	} else {
 		printf("on est dans le puits\n");
+		switch (protocol) {
+			case  0:
+				source_udp(port, host, nb_message, lg_M);
+				break;
+
+			case 1:
+				printf("Coucou");
+				break;
+				
+			default:
+				printf("Coucou");
+				break;
+		}
+	}
 
 	if (nb_message != -1) {
 		if (source == 1)
@@ -74,7 +211,8 @@ void main (int argc, char **argv)
 			printf("nb de tampons à envoyer = 10 par défaut\n");
 		} else
 		printf("nb de tampons à envoyer = infini\n");
-
 	}
+
+	return 0;
 }
 
